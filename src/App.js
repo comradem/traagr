47672b2462d1215029e996f54d6b3512b0424b76
comradem <pperedot@gmail.com>
2018-12-18 08:54:57 +0200

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Cards from "./components/Cards";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Cards>card</Cards>
+      </Container>
     );
   }
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-flow: row nowrap;
+  padding-top: 40px;
+`;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Cards = ({ children }) => <Container>{children}</Container>;
+
+export default Cards;
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100px;
+  background: #aaa;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  padding: 20px;
+  border-radius: 5px;
+  border-color: black;
+  border-width: thick;
+`;
