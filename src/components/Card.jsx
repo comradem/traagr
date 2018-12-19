34700f007c2e3f514aae5b85874e46a53c458714
Pavel Samsonov <pavels@iwobi.co.il>
2018-12-19 18:50:22 +0200

    fonst

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import url from '../images/logo.svg';
+import url from "../images/logo.svg";
 
-const Card = ({ data }) => <Container>{data}<Image src = {url}></Image></Container>;
+const Card = ({ data }) => (
+  <Container>
+    {data} card
+    <Image src={url} />
+    <Description>desc</Description>
+  </Container>
+);
 
 export default Card;
 
@@ -14,7 +20,7 @@ const Container = styled.div`
   box-shadow: 0 5px 20px rgba(0, 0, 0.1, 0.2);
   box-sizing: border-box;
   padding: 10px;
-  border-radius: 5px;
+  border-radius: 10px;
   border-color: black;
   border-width: thick;
 `;
@@ -25,4 +31,17 @@ const Image = styled.img`
   height: 150px;
   margin: 0;
   border-radius: 20px;
+`;
+
+const Description = styled.div`
+  display: inline-block;
+  border-radius: 3px;
+  border-color: black;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: white;
+  color: blue;
+  border: 2px solid white;
+  font: arial  bold 12px/30px Georgia, serif;
 `;
