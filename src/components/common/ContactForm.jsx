f7eccb63107ddf9c0b0a1ca323fcca1cc9f8fe00
Pavel Samsonov <pavels@comradem.local>
2019-02-19 10:12:37 +0200

    added some gui functionality

--- src/components/common/ContactForm.jsx
+++ src/components/common/ContactForm.jsx
@@ -3,15 +3,60 @@ import styled from 'styled-components';
 
 
 class ContactForm extends React.Component {
-    state = {
-        email: this.props.contactEmail,
-        subject: this.props.subjectEmail,
-        content: this.props.contentEmail
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: this.props.contactEmail,
+            subject: this.props.subjectEmail,
+            content: this.props.contentEmail
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    render(){
-        return <div>muahahahah</div>;
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <Container>
+                    <Form>
+                        <label>
+                            Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </Form>
+                </Container>
+            </form>
+        );
     }
 }
 
 export default ContactForm;
+
+const Container = styled.div`
+    width: 90vw;
+    height: 90vw;
+    background: #FFF;
+    box-sizing: border-box;
+    padding: 0 40px;
+`;
+
+const Form = styled.div`
+    width: 30vw;
+    height: 30vw;
+    background: #AAA;
+    display: flex;
+    justify-content: flex-center;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 0 40px;
+`;
