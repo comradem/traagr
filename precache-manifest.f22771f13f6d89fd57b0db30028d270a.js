36cc293f7eab918c99a4032783b0841eae9810fc
comradem <pperedot@gmail.com>
2020-01-08 11:45:57 +0200

    Updates

---
+++ precache-manifest.f22771f13f6d89fd57b0db30028d270a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c3385faa96c27db9e9ea23203f2b79c",
+    "url": "/traarg/static/media/img3.8c3385fa.png"
+  },
+  {
+    "revision": "86ea26b6e8eedc075842",
+    "url": "/traarg/static/css/main.94452d8b.chunk.css"
+  },
+  {
+    "revision": "3d7ff7a7ff6474369829",
+    "url": "/traarg/static/js/1.3d7ff7a7.chunk.js"
+  },
+  {
+    "revision": "08600e57f3e00baba88f",
+    "url": "/traarg/static/js/runtime~main.08600e57.js"
+  },
+  {
+    "revision": "81f5b018b2a9920cc0834c06a40d69c5",
+    "url": "/traarg/static/media/img1.81f5b018.png"
+  },
+  {
+    "revision": "79e36547aec2e4038e2c3c210d9e2921",
+    "url": "/traarg/static/media/img2.79e36547.png"
+  },
+  {
+    "revision": "86ea26b6e8eedc075842",
+    "url": "/traarg/static/js/main.86ea26b6.chunk.js"
+  },
+  {
+    "revision": "4a0624705478afd13b234281055eb2d4",
+    "url": "/traarg/static/media/img4.4a062470.png"
+  },
+  {
+    "revision": "39f9b2653b346c88bccdc4d19302d0d6",
+    "url": "/traarg/static/media/img5.39f9b265.png"
+  },
+  {
+    "revision": "8aa535c4695460ea8432b27e87293b78",
+    "url": "/traarg/static/media/img6.8aa535c4.png"
+  },
+  {
+    "revision": "0cc7e204cfe0b71fe01a7e0cc81613d8",
+    "url": "/traarg/static/media/img7.0cc7e204.png"
+  },
+  {
+    "revision": "f9929e4522e1115afb5a8df5fe8040ef",
+    "url": "/traarg/index.html"
+  }
+];
